5d3f3c00a3318266954bb41248009a28517e1c68
ilya ivanovskiy <domovikx@gmail.com>
2021-04-03 00:49:16 +0300

    chore: add jwt

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,8 +1,10 @@
 import bcryptjs from 'bcryptjs';
+import jsonwebtoken from 'jsonwebtoken';
 
 import { Request, Response } from 'express';
 import { UsersModel } from '../../src/models/users.model';
 import { errorHandler } from '../utils/errorHandler';
+import { JWT_KEY } from '../../src/config/config';
 
 export class UsersController {
   async usersRegister(req: Request, res: Response) {
@@ -32,8 +34,33 @@ export class UsersController {
 
   async usersLogin(req: Request, res: Response) {
     try {
-      const root = await UsersModel.find();
-      res.status(200).json(root);
+      const candidate: any = await UsersModel.findOne({
+        email: req.body.email,
+      });
+
+      if (!candidate) {
+        res.status(404).json({ message: 'Такой пользователь не существует' });
+        return;
+      }
+
+      const password = req.body.password;
+      const comparePassword = bcryptjs.compareSync(
+        password,
+        candidate.password,
+      );
+
+      if (!comparePassword) {
+        res.status(401).json({ message: 'Пароли не совпадают' });
+        return;
+      }
+
+      const token = jsonwebtoken.sign(
+        { email: candidate.email, userID: candidate._id },
+        JWT_KEY,
+        { expiresIn: 360 },
+      );
+
+      res.status(200).json({ token: `Bearer ${token}` });
     } catch (err) {
       errorHandler(res, err);
     }
